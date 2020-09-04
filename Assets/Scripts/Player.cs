9d6ce36aad623c0b65cbc5830e033661d07b63ba
Andrew Lam <drewclam@hotmail.com>
2020-09-03 20:59:27 -0400

    Add player physics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Rigidbody2D rb;
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    bool isGrounded = true; 
+    float xMove;
+    bool shouldJump;
+
+    private void Update() {
+        xMove = Input.GetAxis("Horizontal");
+        shouldJump = Input.GetKeyDown(KeyCode.Space) && isGrounded;
+    }
+
+    private void FixedUpdate() {
+        float xForce = xMove * moveSpeed * Time.deltaTime;
+        Vector2 force = new Vector2(xForce, 0);
+        rb.AddForce(force);
+
+        if (shouldJump) {
+            Debug.Log("Jump");
+            isGrounded = false;
+            rb.AddForce(Vector2.up * jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("Collision enter");
+        if (collision.collider.tag == "Floor") {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        Debug.Log("Collision exit");
+        if (collision.collider.tag == "Floor") {
+            isGrounded = false;
+        }
+    }
+}
