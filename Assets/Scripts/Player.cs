f874a9a5df0c68d73656563b3c1c2e4c654dc2bd
Andrew Lam <drewclam@hotmail.com>
2020-09-05 12:25:43 -0400

    Add sprint

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,34 +5,49 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     public Rigidbody2D rb;
 
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float jumpSpeed = 5f;
-    bool isGrounded = true; 
+    [SerializeField] float baseMoveSpeed = 6000f;
+    [SerializeField] float sprintSpeed = 8000f;
+    [SerializeField] float jumpSpeed = 1000f;
+
+    float moveSpeed;
     float xMove;
-    bool shouldJump;
+    bool grounded = true;
+    bool sprinting = false;
+    bool jumping;
 
     private void Update() {
         xMove = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            shouldJump = true;
-        };
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+            jumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            sprinting = true;
+        } else {
+            sprinting = false;
+        }
     }
 
     private void FixedUpdate() {
+        if (sprinting) {
+            moveSpeed = sprintSpeed;
+        } else {
+            moveSpeed = baseMoveSpeed;
+        }
         float xForce = xMove * moveSpeed * Time.deltaTime;
         Vector2 force = new Vector2(xForce, 0);
         rb.AddForce(force);
 
-        if (shouldJump) {
-            isGrounded = false;
-            shouldJump = false;
+        if (jumping) {
+            grounded = false;
+            jumping = false;
             rb.AddForce(Vector2.up * jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.tag == "Floor") {
-            isGrounded = true;
+            grounded = true;
         }
     }
 }
