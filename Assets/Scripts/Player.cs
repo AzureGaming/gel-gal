90a13b1819d9029b91bcf2f1796078d8a119ac6a
Andrew Lam <drewclam@hotmail.com>
2020-09-05 12:08:04 -0400

    Add jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,17 +13,21 @@ public class Player : MonoBehaviour {
 
     private void Update() {
         xMove = Input.GetAxis("Horizontal");
-        shouldJump = Input.GetKeyDown(KeyCode.Space) && isGrounded;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            shouldJump = true;
+            Debug.Log("Pressed jump");
+        };
     }
 
     private void FixedUpdate() {
-        float xForce = xMove * moveSpeed * Time.deltaTime;
-        Vector2 force = new Vector2(xForce, 0);
-        rb.AddForce(force);
+        //float xForce = xMove * moveSpeed * Time.deltaTime;
+        //Vector2 force = new Vector2(xForce, 0);
+        //rb.AddForce(force);
 
         if (shouldJump) {
-            Debug.Log("Jump");
+            Debug.Log("calculate jump force");
             isGrounded = false;
+            shouldJump = false;
             rb.AddForce(Vector2.up * jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
         }
     }
@@ -32,13 +36,6 @@ public class Player : MonoBehaviour {
         Debug.Log("Collision enter");
         if (collision.collider.tag == "Floor") {
             isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision) {
-        Debug.Log("Collision exit");
-        if (collision.collider.tag == "Floor") {
-            isGrounded = false;
         }
     }
 }
