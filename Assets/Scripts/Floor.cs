be86ee114d15ae9bf263af93b2106646bd0496b9
Andrew Lam <drewclam@hotmail.com>
2020-09-07 17:11:49 -0400

    Add bounce area instantiation

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Floor : MonoBehaviour {
+    public GameObject bounceAreaPrefab;
+
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.CompareTag(GameManager.GEL_TAG)) {
             Destroy(collision.collider.gameObject);
+            Vector2 collisionPos = new Vector3(collision.GetContact(0).point.x, collision.GetContact(0).point.y);
+            Instantiate(bounceAreaPrefab, collisionPos, Quaternion.identity);
         }
     }
 }
