a6691271b5ecaad7fdb6631dc9774b19aaa76c75
Andrew Lam <drewclam@hotmail.com>
2020-09-07 17:46:30 -0400

    Fix bounce area generation

--- Assets/Scripts/Gel.cs
+++ Assets/Scripts/Gel.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Gel : MonoBehaviour {
     public GameObject collisionParticlesPrefab;
+    public GameObject bounceAreaPrefab;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.CompareTag(GameManager.FLOOR_TAG)) {
+            Vector2 collisionPos = new Vector3(collision.GetContact(0).point.x, collision.GetContact(0).point.y);
+
+            Destroy(gameObject);
             Instantiate(collisionParticlesPrefab, transform.position, Quaternion.identity);
+            Instantiate(bounceAreaPrefab, collisionPos, Quaternion.identity);
         }
     }
 }
