194a3889556d8dfdb7ba54d947b0e2f419640bf7
Andrew Lam <drewclam@hotmail.com>
2020-09-10 21:21:06 -0400

    Add button press/unpress

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     public static string GEL_TAG = "Gel";
     public static string FLOOR_TAG = "Floor";
+    public static string BUTTON_COLLIDER = "Button Collider";
+    public static string BUTTON_TRIGGER = "Button Trigger";
 }
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+    private void OnEnable() {
+        ButtonTrigger.OnButtonActivate += HandleButtonPress;
+    }
+
+    private void OnDisable() {
+        ButtonTrigger.OnButtonActivate -= HandleButtonPress;
+    }
+
+    void HandleButtonPress(bool activated) {
+        if (activated) {
+            Debug.Log("Pressed");
+        } else {
+            Debug.Log("Un preesseed");
+        }
+    }
+}
