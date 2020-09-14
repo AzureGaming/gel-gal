3a30d208bb1939532d5969e57f19d18046bfbc66
Andrew Lam <drewclam@hotmail.com>
2020-09-14 00:20:11 -0400

    Update bounce values

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bounce : MonoBehaviour {
-    [SerializeField] float bounceFactor = 2000f;
-    [SerializeField] float gravityApplied = 20f;
+    [SerializeField] float bounceFactor = 4000f;
+    [SerializeField] float gravityApplied = 50f;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         Rigidbody2D rb = collision.collider.attachedRigidbody;
