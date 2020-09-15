63ec88590929307cb7f5b456271ca42207b16b77
Andrew Lam <drewclam@hotmail.com>
2020-09-14 23:47:00 -0400

    Align gel bounce area with surface

--- Assets/Scripts/Gel.cs
+++ Assets/Scripts/Gel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Gel : MonoBehaviour {
     public GameObject collisionParticlesPrefab;
@@ -8,12 +9,20 @@ public class Gel : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.CompareTag(GameManager.FLOOR_TAG)) {
-            Vector2 collisionPos = new Vector3(collision.GetContact(0).point.x, collision.GetContact(0).point.y);
-            Quaternion newRot = Quaternion.FromToRotation(Vector2.up, collision.contacts[0].normal);
-
+            ContactPoint2D contact = collision.GetContact(0);
             Destroy(gameObject);
+            SpawnBounceArea(contact.point.x, contact.point.y, contact.normal);
             Instantiate(collisionParticlesPrefab, transform.position, Quaternion.identity);
-            Instantiate(bounceAreaPrefab, collisionPos, newRot);
         }
+    }
+
+    void SpawnBounceArea(float xPos, float yPos, Vector3 normal) {
+        Vector3 collisionPos = new Vector3(xPos, yPos);
+        Quaternion newRot = Quaternion.FromToRotation(Vector2.up, normal);
+        Vector3 hitPos = Vector3.zero;
+        hitPos.x = xPos - 0.01f * normal.x;
+        hitPos.y = yPos - 0.01f * normal.y;
+
+        Instantiate(bounceAreaPrefab, hitPos, newRot);
     }
 }
