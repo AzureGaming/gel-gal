0e61ac78ffa29cd95aed317f3541ee66960ab99f
Andrew Lam <drewclam@hotmail.com>
2020-09-14 22:26:40 -0400

    Add random gel area sprite start

---
+++ Assets/Scripts/RandomSpriteStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomSpriteStart : MonoBehaviour {
+    public Sprite[] sprites;
+
+    SpriteRenderer spriteR;
+
+    private void Awake() {
+        spriteR = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        int randomIndex = Random.Range(0, sprites.Length);
+        spriteR.sprite = sprites[randomIndex];
+    }
+}
