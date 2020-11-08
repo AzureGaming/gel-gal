a8894ad925baf0bbd645e41ea68df665ed805d91
Andrew Lam <drewclam@hotmail.com>
2020-11-08 14:46:01 -0500

    Add blue button functionality

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    public delegate void ButtonActivated(bool activated);
+    public static event ButtonActivated OnButtonActivate;
+
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public Vector2 colliderSizeUp;
+    public Vector2 colliderSizeDown;
+
+    SpriteRenderer spriteR;
+    CapsuleCollider2D capsuleCollider;
+
+    protected bool isActivated = false;
+
+    private void Awake() {
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        spriteR = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (IsValidTrigger(collision) && !isActivated) {
+            OnButtonActivate?.Invoke(true);
+            isActivated = true;
+            ButtonDown();
+        }
+    }
+
+    public void ButtonUp() {
+        spriteR.sprite = upSprite;
+        capsuleCollider.size = colliderSizeUp;
+    }
+
+    public void ButtonDown() {
+        spriteR.sprite = downSprite;
+        capsuleCollider.size = colliderSizeDown;
+    }
+
+    protected bool IsValidTrigger(Collider2D collision) {
+        if (collision.CompareTag(GameManager.PLAYER_TAG) || collision.CompareTag(GameManager.CRATE)) {
+            return true;
+        }
+        return false;
+    }
+
+    protected virtual void OnButtonActivated(bool activated) {
+        OnButtonActivate?.Invoke(activated);
+    }
+}
--- Assets/Scripts/PressureButton.cs
+++ Assets/Scripts/PressureButton.cs
@@ -2,51 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PressureButton : MonoBehaviour {
-    public delegate void ButtonActivated(bool activated);
-    public static event ButtonActivated OnButtonActivate;
-
-    public Sprite upSprite;
-    public Sprite downSprite;
-    public Vector2 colliderSizeUp;
-    public Vector2 colliderSizeDown;
-
-    public SpriteRenderer spriteR;
-
-    CapsuleCollider2D capsuleCollider;
-
-    private void Awake() {
-        capsuleCollider = GetComponent<CapsuleCollider2D>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (IsValidTrigger(collision)) {
-            OnButtonActivate?.Invoke(true);
-            ButtonDown();
-        }
-    }
-
+public class PressureButton : Button {
     private void OnTriggerExit2D(Collider2D collision) {
-        if (IsValidTrigger(collision)) {
-            OnButtonActivate?.Invoke(false);
+        if (IsValidTrigger(collision) && isActivated) {
+            base.OnButtonActivated(false);
+            isActivated = false;
             ButtonUp();
         }
-    }
-
-    public void ButtonUp() {
-        spriteR.sprite = upSprite;
-        capsuleCollider.size = colliderSizeUp;
-    }
-
-    public void ButtonDown() {
-        spriteR.sprite = downSprite;
-        capsuleCollider.size = colliderSizeDown;
-    }
-
-    bool IsValidTrigger(Collider2D collision) {
-        if (collision.CompareTag(GameManager.PLAYER_TAG) || collision.CompareTag(GameManager.CRATE)) {
-            return true;
-        }
-        return false;
     }
 }
