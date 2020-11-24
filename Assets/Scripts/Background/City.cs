1f93fba6833ca99002cbc7e13eb1b49c498ff7dc
Andrew Lam <drewclam@hotmail.com>
2020-11-23 21:11:57 -0500

    Update city prefab

---
+++ Assets/Scripts/Background/City.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour {
+    public Sprite[] foregrounds;
+    public Sprite[] backgrounds;
+    public Sprite[] middlegrounds;
+    public SpriteRenderer foreground;
+
+    private void Start() {
+        StartCoroutine(UpdateForeground());
+    }
+
+    IEnumerator UpdateForeground() {
+        int index = 0;
+        for (; ; ) {
+            foreground.sprite = foregrounds[index];
+            if (index > foregrounds.Length- 1) {
+                index = 0;
+            } else {
+                index++;
+            }
+        }
+    }
+}
