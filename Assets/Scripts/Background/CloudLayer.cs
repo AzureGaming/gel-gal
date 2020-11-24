13aba9c81b9acab2725de86585ea3affc156c7d7
Andrew Lam <drewclam@hotmail.com>
2020-11-23 20:47:17 -0500

    Update cloud spawning and speed

--- Assets/Scripts/Background/CloudLayer.cs
+++ Assets/Scripts/Background/CloudLayer.cs
@@ -22,7 +22,7 @@ public class CloudLayer : MonoBehaviour {
 
     IEnumerator SpawnCloud() {
         for (; ; ) {
-            float randomWait = Random.Range(1f, 2f);
+            float randomWait = Random.Range(3f, 7f);
             Debug.Log("spawn cloud in " + randomWait + " seconds");
             yield return new WaitForSeconds(randomWait);
             int randomIndex = Random.Range(0, clouds.Length);
